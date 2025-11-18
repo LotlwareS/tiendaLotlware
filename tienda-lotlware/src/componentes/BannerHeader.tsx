defbc606a8ce9a479516ec5e6601fea4b867ae17
Enriquei09 <kike1299.car@gmail.com>
2025-11-17 23:24:47 -0600

    avances

---
+++ tienda-lotlware/src/componentes/BannerHeader.tsx
@@ -0,0 +1,31 @@
+import styles from "./BannerHeader.module.css"
+//import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+export default function BannerHeader(){
+
+    
+
+    return (
+    <div className={styles.BannerHeader}>
+      {/* Fila 1: Título + Ordenar por */}
+        <div className={styles.headerRow}>
+            <h1>LO MEJOR PARA TI</h1>            
+        </div>
+
+      {/* Fila 2: Filtrar por (opcional) */}
+        <div className={styles.container}>
+            <span>Filtrar por</span>
+            <div className={styles.selectWrap}>
+                <select className={styles.select} defaultValue="" >                
+                    <option value="" disabled>
+                    Ordenar por
+                    </option>
+                    <option value="relevancia">Ofertas</option>
+                    <option value="precio_asc">Juegos</option>
+                    <option value="precio_desc">Consolas</option>
+                    <option value="novedades">Accesorios</option>
+                </select>
+            </div>
+        </div>
+    </div>
+  );
+}
--- tienda-lotlware/src/pages/Productos.tsx
+++ tienda-lotlware/src/pages/Productos.tsx
@@ -1,9 +1,104 @@
+import BannerHeader from "../componentes/BannerHeader";
+import CardProducto from "../componentes/CardProducto";
+import styles from "./Productos.module.css";
+import RemoveIcon from '@mui/icons-material/Remove';
+
+
+const Consolas =[
+ 
+    { 
+        id: 1, 
+        name:"consola RX",
+        price:45.22, 
+        urlImage:"https://pub-59ece35300204c2d91a5977706ecf479.r2.dev/consola.jpg" 
+
+    },{ 
+        id: 2, 
+        name:"Gaming mouse", 
+        price:28.99,
+        urlImage:"https://pub-59ece35300204c2d91a5977706ecf479.r2.dev/Gaming%20mouse.jfif" 
+
+    },{ 
+
+        id: 3,
+        name:"Gaming mouse", 
+        price:28.99, 
+        urlImage:"https://pub-59ece35300204c2d91a5977706ecf479.r2.dev/Gaming%20mouse.jfif" 
+
+    },{ 
+        id: 4, 
+        name:"Gaming mouse", 
+        price:28.99,
+        urlImage:"https://pub-59ece35300204c2d91a5977706ecf479.r2.dev/Gaming%20mouse.jfif" 
+
+    },{ 
+
+        id: 5,
+        name:"Gaming mouse", 
+        price:28.99, 
+        urlImage:"https://pub-59ece35300204c2d91a5977706ecf479.r2.dev/Gaming%20mouse.jfif" 
+
+    },{ 
+
+        id: 6,
+        name:"Gaming mouse", 
+        price:28.99, 
+        urlImage:"https://pub-59ece35300204c2d91a5977706ecf479.r2.dev/Gaming%20mouse.jfif" 
+
+    },{ 
+
+        id: 7,
+        name:"Gaming mouse", 
+        price:28.99, 
+        urlImage:"https://pub-59ece35300204c2d91a5977706ecf479.r2.dev/Gaming%20mouse.jfif" 
+
+    } ,{ 
+
+        id: 8,
+        name:"Gaming mouse", 
+        price:28.99, 
+        urlImage:"https://pub-59ece35300204c2d91a5977706ecf479.r2.dev/Gaming%20mouse.jfif" 
+
+    } ,{ 
+
+        id: 9,
+        name:"Gaming mouse", 
+        price:28.99, 
+        urlImage:"https://pub-59ece35300204c2d91a5977706ecf479.r2.dev/Gaming%20mouse.jfif" 
+
+    }   
+];
+
 
 export default function Prodcutos(){
 
     return(
-        <section>
-            <h1>Pagina de Productos</h1>
+        <section className={styles.Productos}>
+            <BannerHeader/>
+            <section className={styles.container}>
+                <article className={styles.list__continer}>
+                    <hr/>
+                    <ul>
+                        <li>Todos</li>
+                        <li>Consolas</li>
+                        <li>Controles</li>
+                        <li>Accesorios</li>
+                        <li>Ofertas</li>
+                        <li>Mas Vendidos</li>
+                    </ul>
+                    <hr/>
+                    <span>Price  <RemoveIcon/></span>
+                </article>
+                <div className={styles.container__Products}>
+                    
+                    {Consolas.map(
+                        (c)=>(
+                            <CardProducto id={c.id} name={c.name} price={c.price} urlImage={c.urlImage}/>
+                        )
+                    )}
+
+                </div>
+            </section>
 
         </section>
     );
